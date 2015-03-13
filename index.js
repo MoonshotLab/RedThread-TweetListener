4aa4954bd1f4342d65aeb93d63bfa0ffd3958e61
Joe Longstreet <joelongstreet@gmail.com>
2015-03-13 08:09:11 -0500

    use a ticks timestamp for easier querying

--- index.js
+++ index.js
@@ -15,22 +15,25 @@ twitter.stream.on('tweet', function(tweet){
   if(tweet.retweeted_status){
     // it's a retweet if retweeted_status object is present
     db.addEventToTweet(tweet.retweeted_status.id, 'retweet', {
-      id    : tweet.id,
-      user  : abbreviate.user(tweet.user)
+      id          : tweet.id,
+      created_at  : new Date(tweet.created_at).getTime(),
+      user        : abbreviate.user(tweet.user)
     }).then(success).fail(error);
   } else if(tweet.in_reply_to_status_id){
     // it's a retweet if in_reply_to_status_id is present
     db.addEventToTweet(tweet.in_reply_to_status_id, 'reply', {
-      id    : tweet.id,
-      text  : tweet.text,
-      user  : abbreviate.user(tweet.user)
+      id          : tweet.id,
+      text        : tweet.text,
+      created_at  : new Date(tweet.created_at).getTime(),
+      user        : abbreviate.user(tweet.user)
     }).then(success).fail(error);
   } else if(twitter.isMention(tweet)){
     // it's a mention if the tweet contains the user we track
     db.addMention({
-      id    : tweet.id,
-      text  : tweet.text,
-      user  : abbreviate.user(tweet.user)
+      id          : tweet.id,
+      text        : tweet.text,
+      created_at  : new Date(tweet.created_at).getTime(),
+      user        : abbreviate.user(tweet.user)
     }).then(success).fail(error);
   } else db.saveTweet(abbreviate.tweet(tweet)).then(success).fail(error);
 });
@@ -39,7 +42,7 @@ twitter.stream.on('tweet', function(tweet){
 // on favorite
 twitter.stream.on('favorite', function(e){
   db.addEventToTweet(e.target_object.id, 'favorite', {
-    created_at  : e.created_at,
+    created_at  : new Date(e.created_at).getTime(),
     user        : abbreviate.user(e.source)
   }).then(success).fail(error);
 });
@@ -48,7 +51,7 @@ twitter.stream.on('favorite', function(e){
 // on follow
 twitter.stream.on('follow', function(e){
   db.addFollow({
-    created_at  : e.created_at,
+    created_at  : new Date(e.created_at).getTime(),
     user        : abbreviate.user(e.source)
   }).then(success).fail(error);
 });
